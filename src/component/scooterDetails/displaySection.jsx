3cf1016451231688162cbd9525d0ceef2c30464b
Sagar soni <Dusksoni@gmail.com>
2024-10-01 03:14:19 +0530

    added comment

--- src/component/scooterDetails/displaySection.jsx
+++ src/component/scooterDetails/displaySection.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import location from "../../assets/images/locationEdit.svg";
 
 function DisplaySection({selectScooter, isImageVisible}) {
+
+  // Format amount in INR currency format
   const formatAmount = (amount) =>
     amount
       ? new Intl.NumberFormat("en-IN", {
@@ -11,73 +13,81 @@ function DisplaySection({selectScooter, isImageVisible}) {
         }).format(amount)
       : "₹0.00";
 
- 
   return (
     <div className="bg-white lg:col-span-5 col-span-11 lg:px-16 md:px-10 px-5 lg:py-16 md:py-13 py-11">
-    <div className="flex flex-col md:gap-x-8 gap-y-8">
-      <div className="flex justify-between">
-        <div className="md:text-[25px] text-[22px]  font-semibold text-black w-fit">
-          Storie
+      <div className="flex flex-col md:gap-x-8 gap-y-8">
+        
+       
+        <div className="flex justify-between">
+          <div className="md:text-[25px] text-[22px] font-semibold text-black w-fit">
+            Storie
+          </div>
+          <div className="flex gap-4 ml-auto">
+            
+            <div
+              className={`lg:h-9 md:h-7 h-5 lg:w-9 md:w-7 w-5 my-auto ring-1 ring-[#E4E4E4] rounded-full`}
+              style={{ background: selectScooter?.color }}
+            />
+            <div className="md:text-sm text-xs text-black my-auto">
+              Sparkling Green
+            </div>
+          </div>
         </div>
-        <div className="flex gap-4 ml-auto">
-          <div
-            className={`lg:h-9 md:h-7 h-5 lg:w-9 md:w-7 w-5 my-auto  ring-1 ring-[#E4E4E4] rounded-full`}
-            style={{ background: selectScooter?.color }}
+
+       
+        <div className="col-span-2 border border-[#00000033]" />
+
+        {/* Scooter image with transition effect */}
+        <div className="col-span-2 mx-auto md:mt-20 mt-8 ">
+          <img
+            src={selectScooter?.image}
+            alt={selectScooter?.name}
+            className={`w-full md:h-auto h-[300px] transition-opacity duration-300 ${
+              isImageVisible ? "opacity-100" : "opacity-0"
+            }`}
           />
-          <div className="md:text-sm text-xs whitespace-nowrap text-black my-auto">
-            Sparkling Green
-          </div>
         </div>
-      </div>
-      <div className="col-span-2 border border-[#00000033]" />
-      <div className="col-span-2 mx-auto md:mt-20 mt-8 ">
-      <img
-          src={selectScooter?.image}
-          alt={selectScooter?.name}
-          className={`w-full md:h-auto h-[300px] transition-opacity duration-300 ${
-            isImageVisible ? "opacity-100" : "opacity-0"
-          }`}
-        />
-      </div>
-      <div className="col-span-2 border border-[#00000033]" />
-      <div className="flex justify-between">
-        <div className="flex flex-col gap-5 w-fit">
-          <div className="text-[10px] text-[#000000E5]">Booking Amount</div>
-          <div className="lg:text-[40px] md:text-3xl text-xl font-semibold leading-[50px]">
-            {formatAmount("2500")}/-
-          </div>
-        </div>
-        <div className="flex flex-col gap-2.5 ml-auto">
-          <div className="flex gap-4 text-[10px] ">
-            <div className="flex flex-col">
-              <span className="text-black text-opacity-60 ">
-                *Ex-showroom price in
-              </span>
-              <span className="text-black text-opacity-30 ">
-                (incl. FAME II Subsidy )
-              </span>
-            </div>
-            <div className="flex h-fit">
-              <select
-                name="location"
-                id="location"
-                className="focus:outline-none select h-fit my-auto"
-              >
-                <option value="New Delhi">New Delhi</option>
-                <option value="Chhattisgarh">Chhattisgarh</option>
-                <option value="Banglore">Banglore</option>
-              </select>
-              <label htmlFor="location" className="w-4 h-5">
-                <img src={location} alt="" className="w-4 h-5" />
-              </label>
+
+       
+        <div className="col-span-2 border border-[#00000033]" />
+
+       
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-5 w-fit">
+            <div className="text-[10px] text-[#000000E5]">Booking Amount</div>
+            <div className="lg:text-[40px] md:text-3xl text-xl font-semibold leading-[50px]">
+              {formatAmount("2500")}/-
             </div>
           </div>
-          <div className="text-[22px]">{formatAmount(117357)}/-</div>
+          
+         
+          <div className="flex flex-col gap-2.5 ml-auto">
+            <div className="flex gap-4 text-[10px]">
+              <div className="flex flex-col">
+                <span className="text-black text-opacity-60">
+                  *Ex-showroom price in
+                </span>
+                <span className="text-black text-opacity-30">
+                  (incl. FAME II Subsidy )
+                </span>
+              </div>
+              <div className="flex h-fit">
+                <select name="location" id="location" className="focus:outline-none select h-fit my-auto">
+                  <option value="New Delhi">New Delhi</option>
+                  <option value="Chhattisgarh">Chhattisgarh</option>
+                  <option value="Banglore">Banglore</option>
+                </select>
+                <label htmlFor="location" className="w-4 h-5">
+                  <img src={location} alt="location icon" className="w-4 h-5" />
+                </label>
+              </div>
+            </div>
+            <div className="text-[22px]">{formatAmount(117357)}/-</div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   )
 }
 
-export default DisplaySection
+export default DisplaySection;
--- src/component/scooterDetails/scooterDetails.jsx
+++ src/component/scooterDetails/scooterDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { IoMdCheckmark } from "react-icons/io";
 import { ScooterData } from "../../data/scooterData";
 import { CgArrowLongRight } from "react-icons/cg";
@@ -6,6 +6,7 @@ import { CgArrowLongRight } from "react-icons/cg";
 function ScooterDetails({ selectScooter, handleColorChange }) {
   return (
     <div className="lg:px-16 md:px-10 px-5 lg:py-16 md:py-13 py-11 lg:col-span-6 col-span-11 h-full flex flex-col">
+      
       <div className="flex gap-10 justify-around mb-10 overflow-x-auto whitespace-nowrap">
         <button className="text-xs font-semibold text-black flex gap-3 ">
           <span>01</span>
@@ -20,10 +21,13 @@ function ScooterDetails({ selectScooter, handleColorChange }) {
           <span>Your Details and preferred location</span>
         </button>
       </div>
+
       <div className="text-3xl font-semibold mb-8">
         Choose <br />a colour
       </div>
-      <div className="bg-white lg:px-16 md:px-10 px-6  py-8 grid grid-cols-2 md:gap-y-16 gap-y-8">
+
+      <div className="bg-white lg:px-16 md:px-10 px-6 py-8 grid grid-cols-2 md:gap-y-16 gap-y-8">
+        
         <div className="flex lg:gap-[30px] md:gap-5 gap-2.5 m-auto">
           <IoMdCheckmark className="text-3xl" />
           <div>
@@ -33,23 +37,28 @@ function ScooterDetails({ selectScooter, handleColorChange }) {
             </div>
           </div>
         </div>
+
         <div className="mx-auto">
           <img src={selectScooter?.image} alt="" className="md:h-[99px] h-[85px]" />
         </div>
-        <div className="col-span-2 flex justify-around ">
+
+        {/* Color options */}
+        <div className="col-span-2 flex justify-around">
           {ScooterData.map((item) => (
             <button
-              className={`lg:h-8 md:h-7 h-6  lg:w-8 md:w-7 w-6 ${
+              key={item.id}
+              className={`lg:h-8 md:h-7 h-6 lg:w-8 md:w-7 w-6 ${
                 item?.id === selectScooter?.id
-                  ? "ring-4 ring-inset ring-black"
+                  ? "ring-4 ring-inset ring-black" // Highlight selected color
                   : "ring-1 ring-[#E4E4E4]"
               }  rounded-full`}
-              style={{ background: item?.color }}
-              onClick={() => handleColorChange(item)}
+              style={{ background: item?.color }} 
+              onClick={() => handleColorChange(item)} // Trigger color change on click
             ></button>
           ))}
         </div>
       </div>
+
       <div className="lg:mt-auto mt-10">
         <button className="flex justify-between w-full md:p-6 p-3 items-center bg-black text-xs text-white font-semibold">
           CONFIRM
